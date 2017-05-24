d5102f8a1c1e15e2c0811e798e8a40e01589e127
Rodrigo Valin <licorna@gmail.com>
2017-05-24 03:26:44 +0100

    IShoppingCart implementation.

--- src/test/java/org/gradle/ShoppingCartTest.java
+++ src/test/java/org/gradle/ShoppingCartTest.java
@@ -1,7 +1,7 @@
 package org.gradle;
 
 import com.xgen.interview.Pricer;
-import com.xgen.interview.ShoppingCart;
+import com.xgen.interview.ShoppingCartNew;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -14,33 +14,33 @@ public class ShoppingCartTest {
 
     @Test
     public void canAddAnItem() {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
 
         sc.addItem("apple", 1);
 
         final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(myOut));
 
-        sc.printReceipt();
+        sc.printReceipt(false);
         assertEquals("apple - 1 - €1.00\n", myOut.toString());
     }
 
     @Test
     public void canAddMoreThanOneItem() {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
 
         sc.addItem("apple", 2);
 
         final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(myOut));
 
-        sc.printReceipt();
+        sc.printReceipt(false);
         assertEquals("apple - 2 - €2.00\n", myOut.toString());
     }
 
     @Test
     public void canAddDifferentItems() {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
 
         sc.addItem("apple", 2);
         sc.addItem("bananna", 1);
@@ -48,22 +48,159 @@ public class ShoppingCartTest {
         final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(myOut));
 
-        sc.printReceipt();
+        sc.printReceipt(false);
         assertEquals("apple - 2 - €2.00\nbananna - 1 - €2.00\n", myOut.toString());
     }
 
-        @Test
+    @Test
     public void doesntExplodeOnMysteryItem() {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
 
         sc.addItem("crisps", 2);
 
         final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(myOut));
 
-        sc.printReceipt();
+        sc.printReceipt(false);
         assertEquals("crisps - 2 - €0.00\n", myOut.toString());
     }
+
+    @Test
+    public void willPrintReceiptsInOrder() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        sc.addItem("bananna", 2);
+        sc.addItem("apple", 3);
+        sc.addItem("crisps", 2);
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        sc.printReceipt(false);
+        assertEquals("bananna - 2 - €4.00\napple - 3 - €3.00\ncrisps - 2 - €0.00\n",
+                     myOut.toString());
+    }
+
+    @Test
+    public void willKeepOrderOnMultipleInsertionsSameItem() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        sc.addItem("bananna", 1);
+        sc.addItem("apple", 3);
+        sc.addItem("crisps", 2);
+        sc.addItem("apple", 2);
+        sc.addItem("bananna", 2);
+
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        sc.printReceipt(true);
+        String details = "bananna - 3 - €6.00\napple - 5 - €5.00\ncrisps - 2 - €0.00\n";
+        String total = "Total - €11.00\n";
+        assertEquals(details + total, myOut.toString());
+    }
+
+    @Test
+    public void printsReceiptTotal() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        sc.addItem("bananna", 2);
+        sc.addItem("apple", 3);
+        sc.addItem("crisps", 2);
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        sc.printReceipt(true);
+        String details = "bananna - 2 - €4.00\napple - 3 - €3.00\ncrisps - 2 - €0.00\n";
+        String total = "Total - €7.00\n";
+        assertEquals(details + total, myOut.toString());
+    }
+
+    @Test
+    public void willNotPrintReceiptIfNotAskedTo() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        sc.addItem("bananna", 2);
+        sc.addItem("apple", 3);
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        sc.printReceipt(false);
+        String details = "bananna - 2 - €4.00\napple - 3 - €3.00\n";
+        assertEquals(details, myOut.toString());
+    }
+
+    @Test
+    public void contentsSizeIsMaintained() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        sc.addItem("bananna", 1);
+        sc.addItem("bananna", 1);
+        sc.addItem("bananna", 1);
+
+        assertEquals(sc.getReceiptSize(), 1);
+    }
+
+    @Test
+    public void contentsSizeIsAmountOfItemsAdded() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        sc.addItem("bananna", 1);
+        sc.addItem("bananna", 1);
+        sc.addItem("bananna", 1);
+        assertEquals(sc.getReceiptSize(), 1);
+
+        sc.addItem("bananna", 1);
+        sc.addItem("bananna", 1);
+        sc.addItem("bananna", 1);
+        assertEquals(sc.getReceiptSize(), 1);
+
+        sc.addItem("apple", 1);
+        sc.addItem("apple", 3);
+        assertEquals(sc.getReceiptSize(), 2);
+
+        sc.addItem("crisps", 1);
+        sc.addItem("apple", 3);
+        assertEquals(sc.getReceiptSize(), 3);
+    }
+
+    @Test
+    public void negativeQtyIsNotAdded() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        sc.addItem("bananna", 0);
+        assertEquals(sc.getReceiptSize(), 0);
+
+        sc.addItem("bananna", -1);
+        assertEquals(sc.getReceiptSize(), 0);
+
+        sc.addItem("bananna", 10);
+        assertEquals(sc.getReceiptSize(), 1);
+    }
+
+    @Test
+    public void canPrintEmptyReceipt() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        sc.printReceipt(false);
+        assertEquals("", myOut.toString());
+    }
+
+    @Test
+    public void canPrintEmptyTotal() {
+        ShoppingCartNew sc = new ShoppingCartNew(new Pricer());
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        sc.printReceipt();
+        assertEquals("Total - €0.00\n", myOut.toString());
+    }
+
 }
-
-
